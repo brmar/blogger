68646b0a738f8fc674800f47e3d9f82f2fb66304
Brian Mar <brianmar17@gmail.com>
2014-08-25 22:24:17 -0700

    finished form-based workflow

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
+#all of the methods in Articles Controller should be the same name
+#as the methods in Controller#Action from rake route
 class ArticlesController < ApplicationController
-  
+  include ArticlesHelper
   def index
     @articles = Article.all
   end
@@ -7,5 +9,42 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    #use 'fail' to check out contents of params
+    #backend
+    @article = Article.new(article_params)
+    @article.save
+    
+    flash.notice = "Article '#{@article.title}' Created!"
+    #redirect after doing backend
+    redirect_to article_path(@article)
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    
+    flash.notice = "Article '#{article.title}' Destroyed!"
+    redirect_to articles_path #uses prefix + _path from rake routes  
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    
+    flash.notice = "Article '#{@article.title}' Updated!"
+    
+    redirect_to article_path(@article)
+  end
+  
   
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
 module ArticlesHelper
+  def article_params
+    #for security reasons, it's not good to blindly save params
+    #so we need to use Strong Parameters like this when making an article
+    params.require(:article).permit(:title, :body)
+  end
 end
